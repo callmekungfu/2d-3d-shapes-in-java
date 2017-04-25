d3b2f56637d6c6e3929b56b914e52df62b9c0f72
Yonglin Wang <wangyonglin1999@gmail.com>
2017-04-24 23:04:38 -0400

    Whoa I wrote so much...
    TODO: override toString methods and finish the irregular pentagonal
    pyramid class.

--- src/com/yonglinwang/Shape.java
+++ src/com/yonglinwang/Shape.java
@@ -8,7 +8,6 @@ import java.awt.Color;
 public abstract class Shape {
     private Color color;
 
-
     public Shape() {
         this(Color.black);
     }
@@ -17,72 +16,47 @@ public abstract class Shape {
         this.color = color;
 
     }
-
     public Color getColor(){
         return color;
     }
-
-
     public void setColor(Color c){
         this.color = c;
     }
-
-
-
 }
 
 abstract class TwoDimensional extends Shape{
-    private double area;
-    private String name;
-    public TwoDimensional(double area, Color color, String name){
+    private static int numOfTwoD = 0;
+    public TwoDimensional(Color color){
         super(color);
-        this.name = name;
-        setArea(area);
+        numOfTwoD++;
     }
     public TwoDimensional(){
-        this(0, Color.black, "undefined");
+        this(Color.black);
     }
-    public String getName(){
-        return name;
-    }
-    public double getArea(){
-        return area;
-    }
-    public void setArea(double width, double length){
-        this.area = width*length;
-    }
-    public void setArea(double area){
-        this.area = area;
+    public abstract double findArea();
+    public abstract double findPerimeter();
+
+    public static int getNumOfTwoD() {
+        return numOfTwoD;
     }
 }
 
 abstract class ThreeDimensional extends Shape{
-    private double surfaceArea;
-    private double volume;
-    private String name;
+    private static int numOfThreeD = 0;
 
-    public ThreeDimensional(double surfaceArea, Color color, double volume, String name){
+
+    public ThreeDimensional(Color color){
         super(color);
-        this.name = name;
-        this.surfaceArea = surfaceArea;
-        this.volume = volume;
     }
-    public double getVolume(){
-        return volume;
-    }
+
     public ThreeDimensional(){
-        this(0, Color.black, 0,"undefined");
+        this(Color.black);
     }
-    public String getName(){
-        return name;
+
+    public static int getNumOfThreeD() {
+        return numOfThreeD;
     }
-    public double getSurfaceArea(){
-        return surfaceArea;
-    }
-    public void setSurfaceArea(double surfaceArea){
-        this.surfaceArea = surfaceArea;
-    }
-    public void setVolume(double volume){
-        this.volume = volume;
-    }
+
+    public abstract double findSurfaceArea();
+    public abstract double findVolume();
 }
--- src/com/yonglinwang/Shapes.java
+++ src/com/yonglinwang/Shapes.java
@@ -7,74 +7,355 @@ import java.awt.Color;
  */
 public class Shapes {
     public static void main(String[] args){
-        Rectangle rect = new Rectangle(10,10, Color.green);
-        rect.setColor(Color.CYAN);
-        System.out.println(rect.getWidth());
     }
 }
 
-class Rectangle extends TwoDimensional{
-    private double width, length;
-    public Rectangle(double width, double length, Color color){
-        super(width*length, color,"Rectangle");
-        this.width = width;
-        this.length = length;
+class Rectangle extends Square{
+    private static int numOfRect = 0;
+    private double sideBravo;
+    public Rectangle(double sideAlpha, double sideBravo, Color color){
+        super(sideAlpha, color);
+        this.sideBravo = sideBravo;
+        numOfRect++;
     }
     public Rectangle(){
-        this(0,0,Color.BLACK);
+        this(0,0, Color.black);
     }
-    public double getWidth(){
-        return width;
+
+    @Override
+    public double findArea() {
+        return sideBravo * getSideAlpha();
     }
-    public double getLength(){
-        return length;
+
+    @Override
+    public double findPerimeter() {
+        return 2 * sideBravo + 2 * getSideAlpha();
     }
-    public void setWidth(double width){
-        this.width = width;
-        setArea(this.width, this.length);
+
+    public void setSideBravo(double sideBravo) {
+        this.sideBravo = sideBravo;
     }
-    public void setLength(double length){
-        this.length = length;
-        setArea(this.width, this.length);
+
+    public double getSideBravo() {
+        return sideBravo;
+    }
+
+    public static int getNumOfRect() {
+        return numOfRect;
     }
 }
 
 class Square extends TwoDimensional{
-    private double sideLength;
-    public Square(double sideLength, Color color){
-        super(sideLength*sideLength, color, "Square");
-        this.sideLength = sideLength;
+    private static int numOfSquare = 0;
+    private double sideAlpha;
+    public Square(double sideAlpha, Color color){
+        super(color);
+        this.sideAlpha = sideAlpha;
+        numOfSquare++;
     }
     public Square(){
         this(0, Color.black);
     }
-    public double getSideLength(){
-        return sideLength;
+    @Override
+    public double findArea() {
+        return sideAlpha * sideAlpha;
     }
-    public void setSideLength(double sideLength){
-        this.sideLength = sideLength;
-        setArea(sideLength,sideLength);
+    @Override
+    public double findPerimeter() {
+        return 4 * sideAlpha;
     }
-
-}
-
-class Rhombus extends TwoDimensional{
-    private double diagonalP, diagnoalQ;
-    public Rhombus(double diagnoalQ, double diagonalP, Color color){
-        super(diagnoalQ * diagonalP / 2, color,"Rhombus");
-        this.diagnoalQ = diagnoalQ;
-        this.diagonalP = diagonalP;
+    public double getSideAlpha(){
+        return sideAlpha;
+    }
+    public void setSideAlpha(double sideAlpha){
+        this.sideAlpha = sideAlpha;
     }
 }
 
-class Trapezoid extends TwoDimensional{
+class Rhombus extends Square{
+    private static int numOfRhombus = 0;
+    private double height;
+    public Rhombus(double sideAlpha, double height, Color color){
+        super(sideAlpha,color);
+        this.height = height;
+        numOfRhombus++;
+    }
+    public Rhombus(){
+        this(0,0, Color.black);
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public static int getNumOfRhombus() {
+        return numOfRhombus;
+    }
 
+    @Override
+    public double findArea() {
+        return getSideAlpha() * getHeight();
+    }
 }
 
-class Kite extends TwoDimensional{
+class IsoscelesTrapezoid extends Parallelogram{
+    private static int numOfIsoscelesTrapezoid = 0;
+    private double sideCharlie;
+    public IsoscelesTrapezoid(double sideAlpha, double sideBravo, double sideCharlie, double height, Color color){
+        super(sideAlpha, sideBravo, height, color);
+        this.sideCharlie = sideCharlie;
+        numOfIsoscelesTrapezoid++;
+    }
+    public IsoscelesTrapezoid(){
+        this(0,0,0,0,Color.black);
+    }
 
+    public void setSideCharlie(double sideCharlie) {
+        this.sideCharlie = sideCharlie;
+    }
+
+    public double getSideCharlie() {
+        return sideCharlie;
+    }
+
+    public static int getNumOfIsoscelesTrapezoid() {
+        return numOfIsoscelesTrapezoid;
+    }
+
+    @Override
+    public double findPerimeter() {
+        return getSideAlpha() + getSideBravo() + 2 * getSideCharlie();
+    }
+
+    @Override
+    public double findArea() {
+        return (getSideAlpha() + getSideBravo()) / 2 * getHeight();
+    }
 }
 
-class parallelogram extends TwoDimensional{
+class Kite extends Square{
+    private static int numOfKite = 0;
+    private double sideBravo;
+    public Kite(double sideAlpha, double sideBravo, Color color){
+        super(sideAlpha,color);
+        this.sideBravo = sideBravo;
+        numOfKite++;
+    }
+    public Kite(){
+        this(0,0,Color.black);
+    }
+
+    public void setSideBravo(double sideBravo) {
+        this.sideBravo = sideBravo;
+    }
+
+    public double getSideBravo() {
+        return sideBravo;
+    }
+
+    public static int getNumOfKite() {
+        return numOfKite;
+    }
+
+    @Override
+    public double findPerimeter() {
+        return 2 * (getSideAlpha() * getSideBravo());
+    }
+}
+
+class Parallelogram extends Rhombus{
+    private static int numOfParallelograms = 0;
+    private double sideBravo;
+    public Parallelogram(double sideAlpha, double sideBravo, double height, Color color){
+        super(sideAlpha,height,color);
+        this.sideBravo = sideBravo;
+        numOfParallelograms++;
+    }
+    public Parallelogram(){
+        this(0,0,0, Color.black);
+    }
+
+    public void setSideBravo(double sideBravo) {
+        this.sideBravo = sideBravo;
+    }
+
+    public double getSideBravo() {
+        return sideBravo;
+    }
+
+    public static int getNumOfParallelograms() {
+        return numOfParallelograms;
+    }
+
+    @Override
+    public double findPerimeter() {
+        return 2 * getSideAlpha() * 2 * getSideBravo();
+    }
+}
+
+class IrregularTrapezoid extends IsoscelesTrapezoid{
+    private static int numOfIrregularTrapezoid = 0;
+    private double sideDelta;
+    public IrregularTrapezoid(double sideAlpha, double sideBravo, double sideCharlie, double sideDelta, Color color){
+        super(sideAlpha,sideBravo,sideCharlie,-1,color);
+        this.sideDelta = sideDelta;
+        numOfIrregularTrapezoid++;
+    }
+
+    public double getSideDelta() {
+        return sideDelta;
+    }
+
+    public void setSideDelta(double sideDelta) {
+        this.sideDelta = sideDelta;
+    }
+
+    public static int getNumOfIrregularTrapezoid() {
+        return numOfIrregularTrapezoid;
+    }
+
+    @Override
+    public double findPerimeter() {
+        return getSideAlpha()+getSideBravo()+getSideCharlie()+getSideDelta();
+    }
+
+    @Override
+    public double findArea() {
+        System.err.println("The program currently cannot find the area of an irregular polygon. -1 is returned.");
+        return -1;
+    }
+}
+
+class Cube extends ThreeDimensional{
+    private static int numOfCubes = 0;
+    private double sideAlpha;
+    public Cube(double sideAlpha, Color color){
+        super(color);
+        this.sideAlpha = sideAlpha;
+        numOfCubes++;
+    }
+    public Cube(){
+        this(0, Color.black);
+    }
+
+    public void setSideAlpha(double sideAlpha) {
+        this.sideAlpha = sideAlpha;
+    }
+
+    public double getSideAlpha() {
+        return sideAlpha;
+    }
+
+    public static int getNumOfCubes() {
+        return numOfCubes;
+    }
+
+    @Override
+    public double findSurfaceArea() {
+        return getSideAlpha() * getSideAlpha() * 6;
+    }
+
+    @Override
+    public double findVolume() {
+        return getSideAlpha() * getSideAlpha() * getSideAlpha();
+    }
+}
+
+class SquareBasedPrism extends Cube{
+    private static int numOfSquareBasedPrism = 0;
+    private double height;
+    public SquareBasedPrism(double sideAlpha, double height, Color color){
+        super(sideAlpha, color);
+        this.height = height;
+        numOfSquareBasedPrism++;
+    }
+    public SquareBasedPrism(){
+        this(0,0,Color.black);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public static int count() {
+        return numOfSquareBasedPrism;
+    }
+
+    @Override
+    public double findSurfaceArea() {
+        return 2 * (getSideAlpha() * getSideAlpha()) + 4 * (getSideAlpha() * getHeight());
+    }
+
+    @Override
+    public double findVolume() {
+        return (getSideAlpha()*getSideAlpha()) * getHeight();
+    }
+}
+
+class RectangularPrism extends SquareBasedPrism{
+    private static int numOfRectangularPrism = 0;
+    private double sideBravo;
+    public RectangularPrism(double sideAlpha, double sideBravo, double height, Color color){
+        super(sideAlpha, height, color);
+        this.sideBravo = sideBravo;
+        numOfRectangularPrism++;
+    }
+    public RectangularPrism(){
+        this(0,0,0,Color.black);
+    }
+
+    public double getSideBravo() {
+        return sideBravo;
+    }
+
+    public void setSideBravo(double sideBravo) {
+        this.sideBravo = sideBravo;
+    }
+
+    public static int getNumOfRectangularPrism() {
+        return numOfRectangularPrism;
+    }
+
+    @Override
+    public double findVolume() {
+        return getSideAlpha() * getSideBravo() * getHeight();
+    }
+
+    @Override
+    public double findSurfaceArea() {
+        return getSideAlpha() * getSideBravo() * 2 + getHeight() * getSideAlpha() * 2 + getHeight() * getSideBravo() * 2;
+    }
+}
+
+class RegularPentagonalPyramid extends RectangularPrism{
+    private static int numOfRegularPentagonalPyramid = 0;
+    public RegularPentagonalPyramid(double sideAlpha, double sideBravo, double height, Color color) {
+        super(sideAlpha, sideBravo, height, color);
+        numOfRegularPentagonalPyramid++;
+    }
+
+    public static int getNumOfRegularPentagonalPyramid() {
+        return numOfRegularPentagonalPyramid;
+    }
+
+    @Override
+    public double findVolume() {
+        return 5 / 12 * Math.tan(54) * getHeight() * (getSideAlpha() * getSideAlpha());
+    }
+
+    @Override
+    public double findSurfaceArea() {
+        return 1.25 * Math.tan(54) * (getSideAlpha() * getSideAlpha()) + 5 * (getSideAlpha() / 2) * Math.sqrt((getHeight() * getHeight())+((getSideAlpha()*Math.tan(54)/2)*(getSideAlpha()*Math.tan(54)/2)));
+    }
+}
+
+class IrregularPentagonalPyramid extends RectangularPrism{
+    private static int numOfIrregularPentagonalPyramid = 0;
+    private double[] sideLengths;
 
 }
